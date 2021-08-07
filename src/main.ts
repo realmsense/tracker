02503ae4dacec6b3a669a41751df145c74ff9ca6
Extacy <git@extacy.cc>
2021-08-07 21:40:22 +1200

    Use fixed auto update, ignore resources & logs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { Runtime, RunOptions } from "nrelay";
 
+const UPDATE_URL = "https://rotmg.extacy.cc/production/client/current/";
+
 const options: RunOptions = {
-    pluginPath: "dist/plugins"
+    pluginPath: "dist/plugins",
+    update: {
+        enabled: true,
+        urls: {
+            build_hash: UPDATE_URL + "build_hash.txt",
+            exalt_version: UPDATE_URL + "exalt_version.txt",
+            objects_xml: UPDATE_URL + "xml/objects.xml",
+            tiles_xml: UPDATE_URL + "xml/tiles.xml",
+        }
+    }
 };
 
 Runtime.start(options);
