493c3d8cba9544e68ef088b276020943cec62cca
Extacy <git@extacy.cc>
2021-09-10 07:02:35 +1200

    Add GetInfo plugin

---
+++ src/plugins/getinfo.ts
@@ -0,0 +1,58 @@
+import { Client, Plugin, PacketHook, Runtime, TextPacket, Logger, LogLevel } from "nrelay";
+import { PLAYER_NAME } from "..";
+
+@Plugin({
+    name: "Get Info",
+    author: "Extacy",
+    enabled: true
+})
+class GetInfoPlugin {
+
+    constructor(
+        private runtime: Runtime,
+    ) {
+
+    }
+
+    @PacketHook()
+    private onTextPacket(textPacket: TextPacket, client: Client) {
+
+        if (textPacket.author != PLAYER_NAME) return;
+        if (!textPacket.text.startsWith("getinfo")) return;
+        if (textPacket.recipient != client.name) return; // must be a pm
+
+        const args = textPacket.text.slice("getinfo".length).trim().split(/ +/);
+        let targetName = args[0];
+        const propName = args[1];
+
+        if (!targetName || !propName) {
+            Logger.log("Get Info", "Usage: getinfo <name|you|me> <prop>", LogLevel.Warning);
+            return;
+        }
+
+        switch (args[0]) {
+            case "me":
+                targetName = textPacket.author;
+                break;
+            case "you":
+                targetName = textPacket.recipient;
+                break;
+            default:
+                break;
+        }
+
+        const entity = client.entityTracker.players.find((value) => value.name == targetName);
+        if (!entity) {
+            Logger.log("Get Info", `Unable to find a player with the name: ${targetName}`, LogLevel.Warning);
+            return;
+        }
+        
+        if (!Object.keys(entity).includes(propName)) {
+            Logger.log("Get Info", `Unknown property: ${propName}`, LogLevel.Warning);
+            return;
+        }
+        
+        Logger.log("Get Info", `The value of "${propName}" is:`, LogLevel.Success);
+        console.log((entity as any)[propName]);
+    }
+}
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,4 @@
+export * from "./getinfo";
 export * from "./test-plugin";
 export * from "./tracker";
 export * from "./tracker/index";
